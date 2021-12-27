55df6756c29c71ac0ef2a84fdb94a969139583dd
张源钢 <6824043@qq.com>
2021-12-27 20:37:28 +0800

    保持对象完整

--- src/main/java/com/restructure/demo/PreserveWholeObject.java
+++ src/main/java/com/restructure/demo/PreserveWholeObject.java
@@ -4,7 +4,7 @@ import com.restructure.demo.bean.TempRange;
 
 /**
  * @author .gang
- * 保持对象完整
+ * 保持对象完整(将一个对象的一堆数据收集起来，并以该对象替换他们，从而来提高函数的可读性)
  * @date 2021/12/27
  */
 public class PreserveWholeObject {
